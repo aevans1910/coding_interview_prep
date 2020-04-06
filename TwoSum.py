a3498eac89f3cf2517899b718f72c08cb2cf6cc5
Ariane Evans <ariane.evans@student.makeschool.com>
2020-04-05 17:19:55 -0700

    [add] second solution

--- TwoSum.py
+++ TwoSum.py
@@ -7,3 +7,13 @@ class Solution:
             for index2 in range(index1 + 1, len(nums)):
                 if nums[index1] + nums[index2] == target:
                     return [index1, index2]
+
+#Hash table solution Time: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}  
+        for index1 in range(len(nums)):
+            needed_num = target - nums[index1]
+            if num_dict.get(needed_num) is not None:
+                return [index1, num_dict[needed_num]]
+            num_dict[nums[index1]] = index1  
