cfc4ca2bf4481cef95a22f373e77c50d3f840f80
Ariane Evans <ariane.evans@student.makeschool.com>
2020-05-06 17:24:11 -0700

    [add] complexity

--- LongestSubstrWithoutRepeatingChar.py
+++ LongestSubstrWithoutRepeatingChar.py
@@ -3,6 +3,9 @@
 # Given a string, find the length of the longest substring without 
 # repeating characters.
 
+# Time complexity: O(n) for going through n characters in the string
+# Space complexity: O(n) for creating a dictionary with n items in it
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Need to go through all of the characters
@@ -10,10 +13,11 @@ class Solution:
         # When a repeating letter appears, the sub string stops
         # Need to compare substrings to find the longest one
         
-        used = {}
-        max_len = start = 0
+        used = {} # O(n) space complexity for a dictionary with n items in it
+        max_len = start = 0 # O(1) space complexity for creating varaible
         
-        for index, char in enumerate(s):
+        for index, char in enumerate(s): # O(n) time complexity for going through
+            #all the characters in the given string
             if char in used and start <= used[char]:
                 start = used[char] + 1
             else:
--- RemoveNthNodeFromEndList.py
+++ RemoveNthNodeFromEndList.py
@@ -1,21 +1,26 @@
 # Leetcode code
 
+# Given a linked list, remove the n-th node from the end of list
+# and return its head.
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-        
+
+# Time complexity: O(2n) as the code needs to go through the linked list twice
+# Space complexity: O(n) as the code creates a list to append n nodes to it      
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         self.tail = None
         
-        return_list = []
+        return_list = [] #O(n) space complexity (n items take n amount of space)
         node = head
         previous_node = None
         node_count = 0
         
-        while node != None:
+        while node != None: #O(n) time to go through n nodes
             node_count += 1
             previous_node = node
             node = node.next
@@ -25,7 +30,7 @@ class Solution:
         node = head
         node_count = 0
         
-        while node != None:
+        while node != None: #O(n) time to go through n nodes
             if node_count != nth_node:
                 return_list.append(node.val)
                 node_count += 1
